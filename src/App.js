59171c6ed936aa04c74e7c5daac445730e8a0914
Julie Nisbet <jmnisbet@gmail.com>
2021-12-01 14:44:08 -0800

    adding counter

--- src/App.js
+++ src/App.js
@@ -14,11 +14,17 @@ import Header from './components/Header/Header'
 function App() {
   const [color, setColor] = useState('red')
   const [type, setType] = useState('square')
+  const [colorCount, setColorCount] = useState(0)
+  const [typeCount, setTypeCount] = useState(0)
 
   return (
     <div className="App">
       <Header {...{ color, type }} />
-      <Shape {...{ color, setColor, type, setType }} />
+      <Shape
+        {...{ color, setColor, type, setType, colorCount, setColorCount, typeCount, setTypeCount }}
+      />
+      <p>You have changed color {colorCount} times.</p>
+      <p>You have changed type {typeCount} times.</p>
     </div>
   )
 }
--- src/components/Shape.js
+++ src/components/Shape.js
@@ -1,13 +1,17 @@
 import './Shape.css'
 
-export default function Shape({ color, type, setColor, setType }) {
+export default function Shape({ color, type, setColor, setType, setTypeCount }) {
   const classes = `shape ${color} ${type}`
+  const handleType = (e) => {
+    setType(e.target.value)
+    setTypeCount((prevState) => prevState + 1)
+  }
   return (
     <>
       <div className={classes}>
         This is a {color} {type}
       </div>
-      <select value={type} onChange={(e) => setType(e.target.value)}>
+      <select value={type} onChange={handleType}>
         <option value="square">Square</option>
         <option value="circle">Circle</option>
       </select>
