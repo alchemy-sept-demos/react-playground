9bfe599df63fb01dda3b8d937f9356e2f31eb5f4
Julie Nisbet <jmnisbet@gmail.com>
2021-12-01 09:43:22 -0800

    adding state to shape

--- src/components/Shape.js
+++ src/components/Shape.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Shape.css'
 
-export default function Shape(props) {
-  console.log(props)
-  const classes = `shape ${props.color} ${props.type}`
+export default function Shape() {
+  const [color, setColor] = useState('red')
+  const [type, setType] = useState('square')
 
+  const classes = `shape ${color} ${type}`
   return (
     <>
       <div className={classes}>
-        This is a {props.color} {props.type}
+        This is a {color} {type}
       </div>
-      {/* <img height="100" src={`${process.env.PUBLIC_URL}/shapes/happy-${props.type}.jpeg`} /> */}
+      <select value={type} onChange={(e) => setType(e.target.value)}>
+        <option value="square">Square</option>
+        <option value="circle">Circle</option>
+      </select>
+      <select value={color} onChange={(e) => setColor(e.target.value)}>
+        <option value="purple">purple</option>
+        <option value="red">red</option>
+        <option value="blue">blue</option>
+        <option value="green">green</option>
+      </select>
     </>
   )
 }
